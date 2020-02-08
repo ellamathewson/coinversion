fe5cceee1c0d08618384be917cab3a84a26d8c6d
Jin Jin Heipler <heipler.jj@gmail.com>
2020-02-08 15:04:00 -0500

    create convert method

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,30 @@
 let app = new Vue({
     el: '#root',
-    data:{
-        apiCall: "https://api.exchangerate-api.com/v4/latest/",
-        startCurr: " ",
+    data: {
+        apiCall: "https://cors-anywhere.herokuapp.com/https://api.exchangerate-api.com/v4/latest/",
+        startCurr: "",
         startAmnt: " ",
         endCurr: "",
         endAmnt: "",
 
     },
-    methods:{
+    methods: {
+        convert() {
+            this.apiCall += `${this.startCurr}`;
+            fetch(this.apiCall)
+                .then(response =>{
+                    if(!response.ok){
+                        throw Error(
+                            `ERROR: ${response.statusText}`
+                        );
+                    }
+                    return response.json();
+                })
+                .then(json => {
+                    console.log(json);
+                })
+
+        }
 
     }
-}); 
+});
