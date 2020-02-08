0822370c6f52b897ae55f62c976d72abdd5c50c4
Jin Jin Heipler <heipler.jj@gmail.com>
2020-02-08 13:55:02 -0500

    added scripts

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+let app = new Vue({
+    el: '#root',
+    data:{
+        apiCall: "https://api.exchangerate-api.com/v4/latest/",
+        startCurr: " ",
+        startAmnt: " ",
+        endCurr: " ",
+        endAmnt: " ",
+
+    },
+    methods:{
+
+    }
+}); 
